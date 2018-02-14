1ed7006fb9d90d1a442ac3bc4771b4f52e0fd4ff
BettinaP <bettinaprophete@gmail.com>
2018-02-13 21:19:10 -0500

    UI set up

--- ToDoList2/FirstViewController.swift
+++ ToDoList2/FirstViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var listTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,14 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "listItemCell")
+        cell.textLabel?.text = "bananas"
+        return cell
+    }
 }
 
